c202c3d1840db7dc109f0de7a95a32fed765c381
ferc255 <alekssslutskiy@yandex.ru>
2018-06-03 14:27:11 +0400

    test

---
+++ foostats/tests/nothing_test.py
@@ -0,0 +1,9 @@
+import unittest
+
+class FlaskrTestCase(unittest.TestCase):
+    def test_nothing(self):
+        self.assertEqual('da', 'da')
+    
+if __name__ == '__main__':
+    unittest.main()
+                        
