c8664a73e01ad9e4bbde3503090f3920eb26341e
ferc255 <alekssslutskiy@yandex.ru>
2018-07-01 15:04:32 +0400

    Calculate all the versions of scoreboard
    Calculate scoreboard after each match and find position of each
    player in that scoreboard.

--- foostats/components/compute.py
+++ foostats/components/compute.py
@@ -7,6 +7,18 @@ import sys
 import collections
 
 
+def get_players_set(matches):
+    """
+    Get list of all the players
+    """
+    players_set = set()
+    for match in matches.values():
+        for color in ['red', 'green', 'blue']:
+            for player in match[color]:
+                players_set.add(player)
+    return players_set
+
+
 def get_total_points(matches):
     """
     Calculates everything so far
@@ -31,8 +43,41 @@ def get_total_points(matches):
                 total_points[player] += points_to_add[color]
 
     result = []
-    for player, score in sorted(total_points.items(), key=lambda x: -x[1]):
+    for player, score in sorted(total_points.items(),
+                                key=lambda x: (-x[1], x[0])):
         result.append([player, score])
+    return result
+
+
+def get_points_history(matches, player_name):
+    """
+    Calculates history of points for certain player
+    """
+    total_points = collections.defaultdict(int)
+    result = []
+    for date, match in sorted(matches.items(), key=lambda x: x[0]):
+        if match['score'][0] == match['score'][1]:
+            points_to_add = {
+                'red': 1,
+                'green': 1,
+                'blue': 1,
+            }
+        else:
+            points_to_add = {
+                'red': 3,
+                'green': 1,
+                'blue': 0,
+            }
+
+        for color in ['red', 'green', 'blue']:
+            for player in match[color]:
+                total_points[player] += points_to_add[color]
+
+        table = sorted(total_points.items(), key=lambda x: (-x[1], x[0]))
+        position = table.index((player_name, total_points[player_name])) + 1
+
+        result.append([date, total_points[player_name], position])
+
     return result
 
 
@@ -40,10 +85,15 @@ def calculate(matches):
     """
     The main function of the component
     """
+    players_set = get_players_set(matches)
     response = {
-        'MAIN': {},
+        key: {} for key in players_set.union(['MAIN'])
     }
+
     response['MAIN']['total_points'] = get_total_points(matches)
+    for player in players_set:
+        response[player]['points_history'] = (
+            get_points_history(matches, player))
 
     return response
 
